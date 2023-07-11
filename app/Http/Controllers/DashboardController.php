67f78c8abf02649de84c8fcdc024425aa0a96861
KNobHack <akudandia7272@gmail.com>
2023-07-11 10:02:52 +0700

    Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use App\Models\DeviceRecord;
 
 class DashboardController extends Controller
 {
     function index()
     {
-        return view('dashboard.index');
+        $data = DeviceRecord::withTrashed()->get();
+
+        $data_masuk = $data->count();
+        $data_terverifikasi = $data
+            ->whereNull('deleted_at')
+            ->whereNotNull('verified_at')
+            ->count();
+        $data_ditolak = $data
+            ->whereNotNull('deleted_at')
+            ->count();
+        $data_belum_diproses = $data
+            ->whereNull('deleted_at')
+            ->whereNull('verified_at')
+            ->count();
+
+        return view('dashboard.index', [
+            'tot_data_masuk' => $data_masuk,
+            'tot_data_terverifikasi' => $data_terverifikasi,
+            'tot_data_ditolak' => $data_ditolak,
+            'tot_data_belum_diproses' => $data_belum_diproses,
+        ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -22,7 +22,7 @@
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                   Total Data Masuk</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $tot_data_masuk }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-database fa-2x text-gray-300"></i>
@@ -40,7 +40,7 @@
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                   Total Data Terverifikasi</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $tot_data_terverifikasi }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-check fa-2x text-gray-300"></i>
@@ -58,7 +58,7 @@
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                   Total Data Ditolak</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $tot_data_ditolak }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-times fa-2x text-gray-300"></i>
@@ -75,11 +75,11 @@
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                  Jumlah akun admin</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                  Total data belum diproses</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $tot_data_belum_diproses }}</div>
               </div>
               <div class="col-auto">
-                <i class="fas fa-user-shield fa-2x text-gray-300"></i>
+                <i class="fas fa-exclamation fa-2x text-gray-300"></i>
               </div>
             </div>
           </div>
@@ -117,17 +117,17 @@
           <!-- Card Body -->
           <div class="card-body">
             <div class="chart-pie pt-4 pb-2">
-              <canvas id="myPieChart"></canvas>
+              <canvas id="persentasiDataPie"></canvas>
             </div>
             <div class="mt-4 text-center small">
-              <span class="mr-2">
-                <i class="fas fa-circle text-primary"></i> Masuk
-              </span>
               <span class="mr-2">
                 <i class="fas fa-circle text-success"></i> Terverifikasi
               </span>
               <span class="mr-2">
                 <i class="fas fa-circle text-danger"></i> Ditolak
+              </span>
+              <span class="mr-2">
+                <i class="fas fa-circle text-warning"></i> Belum Diproses
               </span>
             </div>
           </div>
@@ -137,4 +137,43 @@
 
   </div>
   <!-- /.container-fluid -->
+@endsection
+
+@section('js')
+  <script>
+    // Set new default font family and font color to mimic Bootstrap's default styling
+    Chart.defaults.global.defaultFontFamily = 'Nunito',
+      '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#858796';
+
+    // Pie Chart Example
+    var ctx = document.getElementById("persentasiDataPie");
+    var myPieChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ["Terverifikasi", "Ditolak", "Belum diproses"],
+        datasets: [{
+          data: [{{ $tot_data_terverifikasi }}, {{ $tot_data_ditolak }}, {{ $tot_data_belum_diproses }}],
+          backgroundColor: ['#28a745', '#dc3545', '#ffc107'],
+        }],
+      },
+      options: {
+        maintainAspectRatio: false,
+        tooltips: {
+          backgroundColor: "rgb(255,255,255)",
+          bodyFontColor: "#858796",
+          borderColor: '#dddfeb',
+          borderWidth: 1,
+          xPadding: 15,
+          yPadding: 15,
+          displayColors: false,
+          caretPadding: 10,
+        },
+        legend: {
+          display: false
+        },
+        cutoutPercentage: 80,
+      },
+    });
+  </script>
 @endsection
